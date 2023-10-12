94fa90545b2ad12b0f303a1d32155fb7beda653c
Marcio Souza <contadm305@gmail.com>
2023-10-12 13:44:36 -0300

    ajustando propriedades

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let dropdownToggles = document.querySelectorAll('[data-toggle="dropdown"]');
-    
-    dropdownToggles.forEach(function(toggle) {
-        toggle.addEventListener("click", function(event) {
-            event.preventDefault(); // Previne o comportamento padrão do link
-            
-            let dropdown = this.nextElementSibling;
-            if (dropdown.style.display === "block") {
+
+    function closeAllDropdowns(exceptId) {
+        let dropdowns = document.querySelectorAll('.dropdown-menu');
+        dropdowns.forEach(function(dropdown) {
+            if (dropdown.id !== exceptId) {
                 dropdown.style.display = "none";
+
+                // Assegura que a seta volte ao estado original quando o dropdown é fechado
+                let arrowImg = dropdown.previousElementSibling.querySelector('img');
+                if (arrowImg) {
+                    arrowImg.classList.remove('arrow-rotate');
+                }
+            }
+        });
+    }
+
+    let navItems = document.querySelectorAll('.nav-item');
+    navItems.forEach(function(navItem) {
+        navItem.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            // Remove a classe 'active' de todos os itens do menu
+            navItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+            // Adiciona a classe 'active' ao item do menu que foi clicado
+            this.classList.add('active');
+
+            if (this.hasAttribute("data-toggle")) {
+                let targetId = this.getAttribute("data-target");
+                let dropdown = document.querySelector(targetId);
+                let arrowImg = this.querySelector('img');
+
+                if (dropdown.style.display === "block") {
+                    dropdown.style.display = "none";
+                    arrowImg.classList.remove('arrow-rotate');
+                } else {
+                    closeAllDropdowns(targetId);
+                    dropdown.style.display = "block";
+                    arrowImg.classList.add('arrow-rotate');
+                }
             } else {
-                dropdown.style.display = "block";
+                closeAllDropdowns();
             }
         });
     });
-    
-    // Esconder o dropdown quando clicar fora dele
+
+    // Fechar dropdowns ao clicar fora deles
     document.addEventListener("click", function(event) {
         if (!event.target.closest('.nav-link')) {
-            let dropdowns = document.querySelectorAll('.dropdown-menu');
-            dropdowns.forEach(function(dropdown) {
-                dropdown.style.display = "none";
-            });
+            closeAllDropdowns();
         }
     });
 });
+
+
