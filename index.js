c8bae8af0d53dbd77e746b259dbc4c72feade170
Marcio Souza <contadm305@gmail.com>
2023-10-13 18:48:10 -0300

    adicionando páginas

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     function closeAllDropdowns(exceptId) {
+//         let dropdowns = document.querySelectorAll('.dropdown-menu');
+//         dropdowns.forEach(function(dropdown) {
+//             if (dropdown.id !== exceptId) {
+//                 dropdown.style.display = "none";
+
+//                 // Assegura que a seta volte ao estado original quando o dropdown é fechado
+//                 let arrowImg = dropdown.previousElementSibling.querySelector('img');
+//                 if (arrowImg) {
+//                     arrowImg.classList.remove('arrow-rotate');
+//                 }
+//             }
+//         });
+//     }
+
+//     let navItems = document.querySelectorAll('.nav-item');
+//     navItems.forEach(function(navItem) {
+//         navItem.addEventListener("click", function(event) {
+//             // Verifica se o item do menu é um dropdown
+//             if (this.hasAttribute("data-toggle")) {
+//                 event.preventDefault();  // Previne a ação padrão apenas para dropdowns
+
+//                 let targetId = this.getAttribute("data-target");
+//                 let dropdown = document.querySelector(targetId);
+//                 let arrowImg = this.querySelector('img');
+
+//                 if (dropdown.style.display === "block") {
+//                     dropdown.style.display = "none";
+//                     arrowImg.classList.remove('arrow-rotate');
+//                 } else {
+//                     closeAllDropdowns(targetId);
+//                     dropdown.style.display = "block";
+//                     arrowImg.classList.add('arrow-rotate');
+//                 }
+//             }
+
+//             // Remove a classe 'active' de todos os itens do menu
+//             navItems.forEach(function(item) {
+//                 item.classList.remove('active');
+//             });
+
+//             // Adiciona a classe 'active' ao item do menu que foi clicado
+//             this.classList.add('active');
+
+//         });
+//     });
+
+//     // Fechar dropdowns ao clicar fora deles
+//     document.addEventListener("click", function(event) {
+//         if (!event.target.closest('.nav-link')) {
+//             closeAllDropdowns();
+//         }
+//     });
+// });
+
+// Novo teste
 document.addEventListener("DOMContentLoaded", function() {
 
     function closeAllDropdowns(exceptId) {
@@ -18,17 +76,10 @@ document.addEventListener("DOMContentLoaded", function() {
     let navItems = document.querySelectorAll('.nav-item');
     navItems.forEach(function(navItem) {
         navItem.addEventListener("click", function(event) {
-            event.preventDefault();
-
-            // Remove a classe 'active' de todos os itens do menu
-            navItems.forEach(function(item) {
-                item.classList.remove('active');
-            });
-
-            // Adiciona a classe 'active' ao item do menu que foi clicado
-            this.classList.add('active');
-
+            // Verifica se o item do menu é um dropdown toggle
             if (this.hasAttribute("data-toggle")) {
+                event.preventDefault();  // Previne a ação padrão apenas para os dropdown toggles
+
                 let targetId = this.getAttribute("data-target");
                 let dropdown = document.querySelector(targetId);
                 let arrowImg = this.querySelector('img');
@@ -41,9 +92,15 @@ document.addEventListener("DOMContentLoaded", function() {
                     dropdown.style.display = "block";
                     arrowImg.classList.add('arrow-rotate');
                 }
-            } else {
-                closeAllDropdowns();
             }
+
+            // Remove a classe 'active' de todos os itens do menu
+            navItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+
+            // Adiciona a classe 'active' ao item do menu que foi clicado
+            this.classList.add('active');
         });
     });
 
@@ -54,5 +111,8 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+// Área do Main
 
 
